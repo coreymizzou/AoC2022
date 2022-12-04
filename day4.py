5836fb8592bd6fced66a0ce5270914eca513cd7b
Corey Hughes <50215735+coreymizzou@users.noreply.github.com>
2022-12-04 14:11:27 -0600

    Update day4.py

--- day4.py
+++ day4.py
@@ -1,7 +1,22 @@
+def is_overlapping(x1,x2,y1,y2):
+    return max(x1,y1) <= min(x2,y2)
+
 def overlap(a,b,c,d):
-    if (c >= a) & (c <= b) & (d >= a) & (d <= b):
+    if (c >= a) and (c <= b) and (d >= a) and (d <= b):
         return True
-    elif (a >= c) & (a <= d) & (b >= c) & (b <= d):
+    elif (a >= c) and (a <= d) and (b >= c) and (b <= d):
+        return True
+    else:
+        return False
+        
+def overlapTwo(a,b,c,d):
+    if (c >= a) and (c <= b) and (d >= a) and (d <= b):
+        return True
+    elif (a >= c) and (a <= d) and (b >= c) and (b <= d):
+        return True
+    elif (a == c) or (a == d) or (b == c) or (b == d):
+        return True
+    elif is_overlapping(a,b,c,d) == True:
         return True
     else:
         return False
@@ -9,14 +24,31 @@ def overlap(a,b,c,d):
 
 f = open('input.txt')
 data = f.read().splitlines()
-count = 0
 
-for line in data:
-    x, y = map(str, line.split(','))
-    a, b = map(int, x.split('-'))
-    c, d = map(int, y.split('-'))
+def partOne():
+    count = 0
+    for line in data:
+        x, y = map(str, line.split(','))
+        a, b = map(int, x.split('-'))
+        c, d = map(int, y.split('-'))
     
-    if overlap(a,b,c,d) == True:
-        count += 1
+        if overlap(a,b,c,d) == True:
+            count += 1
 
-print(count)
+    return count
+    
+def partTwo():
+    count = 0
+    for line in data:
+        x, y = map(str, line.split(','))
+        a, b = map(int, x.split('-'))
+        c, d = map(int, y.split('-'))
+    
+        if overlapTwo(a,b,c,d) == True:
+            count += 1
+
+    return count
+    
+    
+print(partOne())
+print(partTwo())
