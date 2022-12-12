32c743a6d94fff4b8e87f3917c4cb28de0d49123
Corey Hughes <50215735+coreymizzou@users.noreply.github.com>
2022-12-12 13:48:02 -0600

    Update day6.py

--- day6.py
+++ day6.py
@@ -1,9 +1,22 @@
 f = open('input.txt')
 data = f.read()
 
-total = range(len(data) - 3)
+total = range(len(data)-3)
 
 for x in total:
-    y = 0
-    if (data[x+3] != data[x+2]) or (data[x+3] != data[x+1]) or (data[x+3] != data[x+1]):
-        print(x)
+    #print(x)
+    one = data[x]
+    two, three, four = data[x+1], data[x+2], data[x+3]
+    firstList = [two, three, four]
+    secondList = [one, three, four]
+    thirdList = [one, two, four]
+    fourthList = [one, two, three]
+    a = any(one == c for c in firstList)
+    b = any(two == c for c in secondList)
+    c = any(three == c for c in thirdList)
+    d = any(four == c for c in fourthList)
+    
+    if (a == False and b == False and c == False and d == False):
+        print(four)
+        print(x+4)
+        break
