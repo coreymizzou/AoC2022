caa9d9e7fcafb7fc84700e10277e1cb0c1a70212
Corey Hughes <50215735+coreymizzou@users.noreply.github.com>
2022-12-12 14:00:21 -0600

    Update day6.py

--- day6.py
+++ day6.py
@@ -1,22 +1,14 @@
-f = open('input.txt')
-data = f.read()
+with open("input.txt", "r") as f:
+	input = f.read()
 
-total = range(len(data)-3)
+def isUnique(string):
+	ls = list(string); ls.sort()
+	lss = list(set(string)); lss.sort()
+	return ls == lss
 
-for x in total:
-    #print(x)
-    one = data[x]
-    two, three, four = data[x+1], data[x+2], data[x+3]
-    firstList = [two, three, four]
-    secondList = [one, three, four]
-    thirdList = [one, two, four]
-    fourthList = [one, two, three]
-    a = any(one == c for c in firstList)
-    b = any(two == c for c in secondList)
-    c = any(three == c for c in thirdList)
-    d = any(four == c for c in fourthList)
-    
-    if (a == False and b == False and c == False and d == False):
-        print(four)
-        print(x+4)
-        break
+def findUniqueSubstring(string, offset = 4):
+	for i in range(len(string)):
+		if isUnique(input[i:i + offset]):
+			return i + offset
+
+print(f"Answer 1: {findUniqueSubstring(input)}\nAnswer 2: {findUniqueSubstring(input, offset = 14)}")
